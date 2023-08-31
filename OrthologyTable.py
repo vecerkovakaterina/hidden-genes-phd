a8b831e36a05af9d864888118f5dd1f9be88ada3
vecerkovakaterina <vecerkovakaterina1@gmail.com>
2023-08-31 14:20:01 +0200

    Add function to parse the orthology table

--- OrthologyTable.py
+++ OrthologyTable.py
@@ -1,3 +1,32 @@
+from pathlib import Path
+import pandas as pd
+
+
 class OrthologyTable:
-    def __init__(self):
-        pass
+    def __init__(self, path_to_orthology_table):
+        self.table_file = path_to_orthology_table
+        self.orthology_df = self.parse_orthology_table()
+
+    @classmethod
+    def file_exists(cls, file):
+        return Path(file).is_file()
+
+    def parse_orthology_table(self):
+        if self.file_exists(self.table_file):
+            suffix = Path(self.table_file).suffix
+            if suffix == ".csv":
+                return pd.read_csv(
+                    self.table_file, sep=",", header=0, index_col=0, low_memory=False
+                )
+            elif suffix == ".tsv":
+                return pd.read_csv(
+                    self.table_file, sep="\t", header=0, index_col=0, low_memory=False
+                )
+            elif suffix == ".xlsx":
+                return pd.read_excel(self.table_file, header=0, index_col=0)
+            else:
+                raise ValueError(
+                    "Invalid orthology table format. Accepted formats .csv, .tsv, .xlsx."
+                )
+        else:
+            raise FileNotFoundError(f"File {self.table_file} does not exist.")
