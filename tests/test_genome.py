4e1f66a25c0d22560d07bf0e5d409c04968814df
vecerkovakaterina <vecerkovakaterina1@gmail.com>
2023-09-05 14:00:01 +0200

    Replace assembly with annotation in test_genome.py

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -1,12 +1,12 @@
 from hidden_genes_phd.Genome import Genome
 from pathlib import Path
 
-valid_assembly_name = "heterocephalus_glaber_female"
-invalid_assembly_name = "heterocephalus_glaber"
+valid_annotation_name = "heterocephalus_glaber_female"
+invalid_annotation_name = "heterocephalus_glaber"
 
 
 def test_class_attributes():
-    g = Genome(valid_assembly_name)
+    g = Genome(valid_annotation_name)
     assert g.kind == "vertebrate"
     assert g.ensembl_release == 109
 
@@ -17,12 +17,12 @@ def test_class_attribute_genomes_dict():
 
 
 def test_instance_attribute_species():
-    g = Genome(valid_assembly_name)
-    assert g.species == valid_assembly_name
+    g = Genome(valid_annotation_name)
+    assert g.species == valid_annotation_name
 
 
 def test_instance_attribute_ftp_link():
-    g = Genome(valid_assembly_name)
+    g = Genome(valid_annotation_name)
     assert (
         g.ftp_link
         == "http://ftp.ensembl.org/pub/release-109/gtf/heterocephalus_glaber_female/Heterocephalus_glaber_female.HetGla_female_1.0.109.gtf.gz"
@@ -30,15 +30,15 @@ def test_instance_attribute_ftp_link():
 
 
 def test_instance_attribute_genome_file():
-    g = Genome(valid_assembly_name)
+    g = Genome(valid_annotation_name)
     assert Path("genomes", g.species + ".gtf.gz").exists() is True
 
 
 def test_genome_annotation_unpacked():
-    g = Genome(valid_assembly_name)
+    g = Genome(valid_annotation_name)
     assert Path("genomes", g.species + ".gtf").exists() is True
 
 
-def test_class_method_assembly_name_exists():
-    assert Genome.assembly_name_exists(valid_assembly_name) is True
-    assert Genome.assembly_name_exists(invalid_assembly_name) is False
+def test_class_method_annotation_name_exists():
+    assert Genome.annotation_name_exists(valid_annotation_name) is True
+    assert Genome.annotation_name_exists(invalid_annotation_name) is False
