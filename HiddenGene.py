a3a1db9de2dc91522ec4cadec89039c0429f0bda
vecerkovakaterina <vecerkovakaterina1@gmail.com>
2024-01-24 11:33:49 +0100

    Fix bug

--- HiddenGene.py
+++ HiddenGene.py
@@ -12,7 +12,9 @@ class HiddenGene:
         self.orthology_group = orthology_group
         self.missing_from_genome = genome
         self.left_neighbor = None
+        self.left_neighbor_orthology_group = None
         self.right_neighbor = None
+        self.right_neighbor_orthology_group = None
         self.coordinates = None
         self.sequence = None
         self.overlaps_with = None
@@ -29,9 +31,11 @@ class HiddenGene:
         if side not in valid_sides:
             raise ValueError(f"Side must be of {valid_sides}")
 
-    def get_n_neighbors_side(self, genomes, n_neighbors_to_search=10, side=None):
+    def get_n_neighbors_side(
+        self, genomes, orthology_table, n_neighbors_to_search=10, side=None
+    ):
         self.validate_side(side=side)
-        neighbors_dfs = []
+        neighbors_lists = []
         for i, species in enumerate(self.orthology_group.species_with_ortholog):
             orthologs = OrthologyGroup.drop_nans_from_list(
                 self.orthology_group.orthologs
@@ -43,28 +47,48 @@ class HiddenGene:
                 .select(pl.col("index"))
                 .item()
             )
-            if side == "left":
-                neighbors_df = genomes[species].annotation.df.slice(
-                    offset=index_of_ortholog_in_annotation - n_neighbors_to_search,
-                    length=n_neighbors_to_search,
-                )
-            elif side == "right":
-                neighbors_df = genomes[species].annotation.df.slice(
-                    offset=index_of_ortholog_in_annotation - n_neighbors_to_search,
-                    length=n_neighbors_to_search,
-                )
-            neighbors_dfs.append(neighbors_df)
-        return neighbors_dfs
 
-    def find_most_common_neighbor_side(self, neighbors_df, orthology_groups, side=None):
-        # lists of neighbors as ensembl ids
-        self.validate_side(side=side)
-        neighbors_ensembl_ids = []
-        for df in neighbors_df:
-            neighbors_ensembl_ids.append(
-                df.select(pl.col("ensembl_id")).to_series().to_list()
-            )
+            neighbors = []
+            counter = 1
+            while len(neighbors) < n_neighbors_to_search:
+                if side == "left":
+                    next_gene_ensembl_id = (
+                        genomes[species]
+                        .annotation.df.slice(
+                            offset=index_of_ortholog_in_annotation - counter,
+                            length=1,
+                        )
+                        .select(pl.col("ensembl_id"))
+                        .item()
+                    )
+                elif side == "right":
+                    next_gene_ensembl_id = (
+                        genomes[species]
+                        .annotation.df.slice(
+                            offset=index_of_ortholog_in_annotation + counter,
+                            length=1,
+                        )
+                        .select(pl.col("ensembl_id"))
+                        .item()
+                    )
+                # check if next gene present in annotation with hidden gene
+                if (
+                    orthology_table.get_ensembl_id_of_ortholog_in_species(
+                        next_gene_ensembl_id,
+                        species,
+                        self.missing_from_genome.species_name,
+                    )
+                    != "nan"
+                ):
+                    neighbors.append(next_gene_ensembl_id)
+                counter += 1
+            neighbors_lists.append(neighbors)
 
+        return neighbors_lists
+
+    def find_most_common_neighbor_side(
+        self, neighbors_ensembl_ids, orthology_groups, side=None
+    ):
         # reverse list if side left
         if side == "left":
             neighbors_ensembl_ids = [x[::-1] for x in neighbors_ensembl_ids]
@@ -75,20 +99,27 @@ class HiddenGene:
                 for orthology_group in orthology_groups:
                     if neighbor in orthology_group.orthologs:
                         neighbors_ensembl_ids[i][j] = orthology_group
+                if not isinstance(
+                    neighbors_ensembl_ids[i][j],
+                    OrthologyGroup,
+                ):
+                    neighbors_ensembl_ids[i][j] = None
 
         counter = Counter(neighbors_ensembl_ids[0])
         for i in neighbors_ensembl_ids[1:]:
             counter.update(i)
 
-        occurences = counter.most_common()
+        occurences = (
+            counter.most_common()
+        )  # TODO more equally frequent ones, can check for the closest one from the most frequent ones
         if len(occurences) > 0:
-            return occurences[0][0]
+            # take another one if the most frequent one is None
+            if occurences[0][0] is None and len(occurences) > 1:
+                return occurences[1][0]
+            else:
+                return occurences[0][0]
         else:
             return None
-
-    # def set_neighbor_genes(self):
-    #     self.left_neighbor = self.find_neighbor_gene()
-    #     self.right_neighbor = self.find_neighbor_gene()
 
     def find_overlapping_annotation(self):
         # TODO
