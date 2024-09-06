c46c54e4a8e131c24a07c6505289b35f840b4478
vecerkovakaterina <vecerkovakaterina1@gmail.com>
2024-09-06 10:33:37 +0200

    Parallelize scoring orthology groups

--- OrthologyGroups.py
+++ OrthologyGroups.py
@@ -9,10 +9,21 @@ class OrthologyGroups:
         for group in self.orthology_groups_list:
             yield group
 
-    def score_all_groups(self, orthology_table):  # TODO include taxon score
-        for group in self:
-            group.assign_score(orthology_table)
-        return self
+    # def score_all_groups(self, orthology_table):  # TODO include taxon score
+    #     for group in self:
+    #         group.assign_score(orthology_table)
+    #     return self
+
+    @staticmethod
+    def score_orthology_group(orthology_group, orthology_table):
+        orthology_group.assign_score(orthology_table)
+
+    def rank_groups_by_score(self, orthology_table):  # TODO parallelize
+        # self.score_all_groups(orthology_table)
+        # self.orthology_groups_list.sort(key=lambda x: x.score, reverse=True)
+        # return self
+        with ThreadPoolExecutor() as executor:
+            executor.map(OrthologyGroups.score_orthology_group, self.orthology_groups_list)
 
     def remove_full_groups(self):
         with ThreadPoolExecutor() as executor:
@@ -23,11 +34,6 @@ class OrthologyGroups:
                 if future.result():
                     self.orthology_groups_list.remove(group)
 
-        return self
-
-    def rank_groups_by_score(self, orthology_table):  # TODO parallelize
-        self.score_all_groups(orthology_table)
-        self.orthology_groups_list.sort(key=lambda x: x.score, reverse=True)
         return self
 
     @staticmethod
